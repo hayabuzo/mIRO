1f8fb61629ed3ade3b0d321da3a76b7f5ec4d92d
Sergey Egorov <thehaha@yandex.ru>
2024-05-19 23:53:34 +0300

    fix buildShader error

--- setup.js
+++ setup.js
@@ -124,71 +124,6 @@ function setup() {                                           // preparing sketch
     if (profile.forcing) { save('','?.txt'); save('','?.txt'); }
     
   }
-
-  const buildShader = () => {                                         // function that builds array of fragment shaders from the text
-
-    const revealName = () => {
-      glsl.code = txtar.value();                                              // get current filter code from textarea
-      for (let i=0; i<glsl.names.length; i++) {                               // for every name of preset in list of names
-        let name = new RegExp("# "+glsl.names[i]+" #","g");                   // create regEx with macro syntax
-        let code = glsl.parray[i].split("@")[1];                              // take the preset code
-        glsl.code = glsl.code.replace(name," // "+glsl.names[i]+" "+code);    // and replace preset macro with code
-      }	
-      let reg = new RegExp("@","g"); return glsl.code.replace(reg,"\n@");     // add line break for saving function
-    }
-  
-    revealName();
-    
-    let ctrl_reg = new RegExp("[ABCabc01XxYy][ABCabc01XxYy][ABCabc01XxYy][ABCabc01XxYy][ABCabc01XxYy]"+" #","g");         // create regEx for controls
-    let ctrl_arr = [];                                                      // create array for controls rebinds in code
-    ctrl_arr = glsl.code.match(ctrl_reg);                                   // search for rebinds and make array of it
-    glsl.code = glsl.code.replace(ctrl_reg,"");                             // delete rebinds from code
-  
-    let shaders_array = glsl.code.split("@");                               // split the code with @ symbols to separate fragment shaders
-    glsl.frags = []; for (let i=1; i<shaders_array.length; i++) {           // generate array of fragment shaders
-      if (ctrl_arr!=null) if (ctrl_arr[i-1]!=null) {                        // if this shader have control rebinds
-        ctrl_arr[i-1] = ctrl_arr[i-1].split("");
-        for (let j in ctrl_arr[i-1]) {
-          if (ctrl_arr[i-1][j]=="a") ctrl_arr[i-1][j] = "(1.0-A)";
-          if (ctrl_arr[i-1][j]=="b") ctrl_arr[i-1][j] = "(1.0-B)";
-          if (ctrl_arr[i-1][j]=="c") ctrl_arr[i-1][j] = "(1.0-C)";
-          if (ctrl_arr[i-1][j]=="0") ctrl_arr[i-1][j] = "0.0";
-          if (ctrl_arr[i-1][j]=="1") ctrl_arr[i-1][j] = "1.0";
-          if (ctrl_arr[i-1][j]=="X") ctrl_arr[i-1][j] = "MX";
-          if (ctrl_arr[i-1][j]=="Y") ctrl_arr[i-1][j] = "MY";
-          if (ctrl_arr[i-1][j]=="x") ctrl_arr[i-1][j] = "(1.0-MX)";
-          if (ctrl_arr[i-1][j]=="y") ctrl_arr[i-1][j] = "(1.0-MY)";
-        }
-        shaders_array[i] = shaders_array[i]                                 // replace control chars in code with new ones
-        .replace(/\bA\b/g,"replace_a")
-        .replace(/\bB\b/g,"replace_b")
-        .replace(/\bC\b/g,"replace_c")
-        .replace(/replace_a/g,ctrl_arr[i-1][0])
-        .replace(/replace_b/g,ctrl_arr[i-1][1])
-        .replace(/replace_c/g,ctrl_arr[i-1][2])
-        .replace(/MX/g,ctrl_arr[i-1][3])
-        .replace(/MY/g,ctrl_arr[i-1][4])
-        ;
-      }
-      //print(shaders_array[i]);
-      glsl.frags.push(glsl.uniforms + glsl.library + ` void main() { ` + shaders_array[i] + `gl_FragColor.a *= alpha;` + ` } `);  // build the shader
-    }
-    
-    //print(glsl.frags);
-    
-    // if there are shader control variables in the code, turn on these controls in gui
-    shaders_array = shaders_array.join('');
-    glsl.mx = str(shaders_array).search(/\bMX\b/) > 0 ? true : false;
-    glsl.my = str(shaders_array).search(/\bMY\b/) > 0 ? true : false;
-    glsl.a  = str(shaders_array).search(/\bA\b/)  > 0 ? true : false;
-    glsl.b  = str(shaders_array).search(/\bB\b/)  > 0 ? true : false;
-    glsl.c  = str(shaders_array).search(/\bC\b/)  > 0 ? true : false;
-    if (profile.keymode) { glsl.a = true; glsl.b = true; glsl.c = true; }
-    glsl.n  = int(glsl.a) + int(glsl.b) + int(glsl.c);
-  
-  }
-
-
   
   pixelDensity(1);                                           // set the same density for all devices to prevent over-resolution
   cnv = createCanvas(windowWidth, windowHeight);             // create canvas with full window size
@@ -215,4 +150,68 @@ const load_preset = () => {                                        // when loadi
 	if (gui.frame=="F1") gui.frame="F1L";                         // recalculate buttons size to align controls
   for (let i in gui.trig) { gui.trig[i] = false; }              // reset shader controls
 	document.getElementById('mySel').blur();                      // set the focus out of selector
+}
+
+
+const buildShader = () => {                                         // function that builds array of fragment shaders from the text
+
+  const revealName = () => {
+    glsl.code = txtar.value();                                              // get current filter code from textarea
+    for (let i=0; i<glsl.names.length; i++) {                               // for every name of preset in list of names
+      let name = new RegExp("# "+glsl.names[i]+" #","g");                   // create regEx with macro syntax
+      let code = glsl.parray[i].split("@")[1];                              // take the preset code
+      glsl.code = glsl.code.replace(name," // "+glsl.names[i]+" "+code);    // and replace preset macro with code
+    }	
+    let reg = new RegExp("@","g"); return glsl.code.replace(reg,"\n@");     // add line break for saving function
+  }
+
+  revealName();
+  
+  let ctrl_reg = new RegExp("[ABCabc01XxYy][ABCabc01XxYy][ABCabc01XxYy][ABCabc01XxYy][ABCabc01XxYy]"+" #","g");         // create regEx for controls
+  let ctrl_arr = [];                                                      // create array for controls rebinds in code
+  ctrl_arr = glsl.code.match(ctrl_reg);                                   // search for rebinds and make array of it
+  glsl.code = glsl.code.replace(ctrl_reg,"");                             // delete rebinds from code
+
+  let shaders_array = glsl.code.split("@");                               // split the code with @ symbols to separate fragment shaders
+  glsl.frags = []; for (let i=1; i<shaders_array.length; i++) {           // generate array of fragment shaders
+    if (ctrl_arr!=null) if (ctrl_arr[i-1]!=null) {                        // if this shader have control rebinds
+      ctrl_arr[i-1] = ctrl_arr[i-1].split("");
+      for (let j in ctrl_arr[i-1]) {
+        if (ctrl_arr[i-1][j]=="a") ctrl_arr[i-1][j] = "(1.0-A)";
+        if (ctrl_arr[i-1][j]=="b") ctrl_arr[i-1][j] = "(1.0-B)";
+        if (ctrl_arr[i-1][j]=="c") ctrl_arr[i-1][j] = "(1.0-C)";
+        if (ctrl_arr[i-1][j]=="0") ctrl_arr[i-1][j] = "0.0";
+        if (ctrl_arr[i-1][j]=="1") ctrl_arr[i-1][j] = "1.0";
+        if (ctrl_arr[i-1][j]=="X") ctrl_arr[i-1][j] = "MX";
+        if (ctrl_arr[i-1][j]=="Y") ctrl_arr[i-1][j] = "MY";
+        if (ctrl_arr[i-1][j]=="x") ctrl_arr[i-1][j] = "(1.0-MX)";
+        if (ctrl_arr[i-1][j]=="y") ctrl_arr[i-1][j] = "(1.0-MY)";
+      }
+      shaders_array[i] = shaders_array[i]                                 // replace control chars in code with new ones
+      .replace(/\bA\b/g,"replace_a")
+      .replace(/\bB\b/g,"replace_b")
+      .replace(/\bC\b/g,"replace_c")
+      .replace(/replace_a/g,ctrl_arr[i-1][0])
+      .replace(/replace_b/g,ctrl_arr[i-1][1])
+      .replace(/replace_c/g,ctrl_arr[i-1][2])
+      .replace(/MX/g,ctrl_arr[i-1][3])
+      .replace(/MY/g,ctrl_arr[i-1][4])
+      ;
+    }
+    //print(shaders_array[i]);
+    glsl.frags.push(glsl.uniforms + glsl.library + ` void main() { ` + shaders_array[i] + `gl_FragColor.a *= alpha;` + ` } `);  // build the shader
+  }
+  
+  //print(glsl.frags);
+  
+  // if there are shader control variables in the code, turn on these controls in gui
+  shaders_array = shaders_array.join('');
+  glsl.mx = str(shaders_array).search(/\bMX\b/) > 0 ? true : false;
+  glsl.my = str(shaders_array).search(/\bMY\b/) > 0 ? true : false;
+  glsl.a  = str(shaders_array).search(/\bA\b/)  > 0 ? true : false;
+  glsl.b  = str(shaders_array).search(/\bB\b/)  > 0 ? true : false;
+  glsl.c  = str(shaders_array).search(/\bC\b/)  > 0 ? true : false;
+  if (profile.keymode) { glsl.a = true; glsl.b = true; glsl.c = true; }
+  glsl.n  = int(glsl.a) + int(glsl.b) + int(glsl.c);
+
 }
